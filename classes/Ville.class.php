732ea416f783824690a9203ea41ffcfc8b9b230a
Unknown <jarreau.maxime@gmail.com>
2018-10-16 17:06:31 +0200

    modif class ville

--- classes/Ville.class.php
+++ classes/Ville.class.php
@@ -1,4 +1,15 @@
 <?php
 class Ville{
 	
+	private $vil_nom;
+	public function __construct($newVille){
+		$this->vil_nom=$newVille;
+	}
+	public function setNomVille($newVille){
+		$this->vil_nom=$newVille;
+		
+	}
+	public function getNomVille(){
+		return $this->vil_nom;
+	}
 }
