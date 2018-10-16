6ddb1359a0fe730ef35b9df95f490eab9ff69060
BastLast <bastien.last87@gmail.com>
2018-10-16 16:43:22 +0200

    Ajout formulaire d'ajout de ville

--- include/pages/ajouterVille.inc.php
+++ include/pages/ajouterVille.inc.php
@@ -1,1 +1,15 @@
 <h1>Ajouter une ville</h1
+
+  <form action="#" id="SaisieNomVille" method="post">
+    Nom :<input type="text" name="FormPrenomCli" size="4">
+    <input type="submit" value="Ok" />
+  </form>
+
+  <?php
+  $ville = new Ville($_POST);
+  $db = new Mypdo();
+  $villeManager = new VilleManager($db);
+  $villeManager -> addville($ville);
+
+
+  ?>
