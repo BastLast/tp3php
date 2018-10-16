b07318c13be56d1506a989793d6ac74cbdd82907
BastLast <bastien.last87@gmail.com>
2018-10-16 17:29:16 +0200

    amelioration du formulaire ajouterVille

--- include/pages/ajouterVille.inc.php
+++ include/pages/ajouterVille.inc.php
@@ -1,15 +1,18 @@
-<h1>Ajouter une ville</h1
-
-  <form action="#" id="SaisieNomVille" method="post">
+<h1>Ajouter une ville</h1>
+<?php
+if (empty($_POST)){
+  ?>
+  <form action="#" id="test" method="post">
     Nom :<input type="text" name="FormPrenomCli" size="4">
     <input type="submit" value="Ok" />
   </form>
-
+  <?php
+}else{
+  ?>
   <?php
   $ville = new Ville($_POST);
   $db = new Mypdo();
   $villeManager = new VilleManager($db);
   $villeManager -> addville($ville);
-
-
+}
   ?>
