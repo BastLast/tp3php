26536ae3bdc0b25ba3ebc77e6985f1c3c3e3bec7
BastLast <bastien.last87@gmail.com>
2018-10-24 14:33:43 +0200

    Ajout de la gestion du numéro des villes

--- classes/Ville.class.php
+++ classes/Ville.class.php
@@ -1,14 +1,36 @@
 <?php
 class Ville{
-	
+
+	private $vil_num;
 	private $vil_nom;
-	public function __construct($newVille){
-		$this->vil_nom=$newVille;
+
+	public function __construct($valeurs = array()){
+		if (!empty($valeurs)) {
+			$this->affecte($valeurs);
+		}
 	}
-	public function setNomVille($newVille){
-		$this->vil_nom=$newVille;
-		
+
+	public function affecte($donnees){
+		foreach ($donnees as $attribut => $valeur) {
+
+			switch($attribut){
+
+				case 'vil_num':  $this->setNumVille($valeur);
+				break;
+				case 'vil_nom':  $this->setNomVille($valeur);
+				break;
+			}
+		}
 	}
+
+	public function setNumVille($numVille){
+		$this->vil_nom=$numVille;
+	}
+
+	public function setNomVille($nomVille){
+		$this->vil_nom=$nomVille;
+	}
+
 	public function getNomVille(){
 		return $this->vil_nom;
 	}
