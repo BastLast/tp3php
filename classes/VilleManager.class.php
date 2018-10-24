455f1a4539549536b7968beea92441cef7cc8aa9
Unknown <jarreau.maxime@gmail.com>
2018-10-24 13:43:57 +0200

    modif villemanagement

--- classes/VilleManager.class.php
+++ classes/VilleManager.class.php
@@ -1,6 +1,13 @@
 <?php
 class VilleManager{
+	$db=mysqli_connect("localhost","bd","bede");
+	mysqli_select_db($db,"covoit");
 	
+	public function inserer(){
 	$reqIns="insert".$vil_nom." from ville ";
+	$exeIns=mysqli_query($db,$reqIns);
+	
+	}
 	$reqSel="select vil_nom from ville";
+	
 }
