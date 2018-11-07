fb9d8b026a06082047fd6652dd3a311cd04e7d51
Unknown <jarreau.maxime@gmail.com>
2018-11-07 17:24:14 +0100

    écriture de addPersonne

--- classes/PersonneManager.class.php
+++ classes/PersonneManager.class.php
@@ -1,4 +1,24 @@
 <?php
 class PersonneManager{
+	public function __construct($db){
+		$this->db = $db;
+	}
+	
+	//fonction permettant d'jouter une ville 
+	public function addPersonne($Personne){
+		$req=$this ->db->prepare
+		('INSERT INTO Personne (per_num,per_nom,per_prenom,per_tel,per_mail,per_login,per_pwd) 
+		VALUES (:num,:nom,:prenom,:tel,:mail,:login,:pwd)')
+		
+		$req ->bindValue(':num',$Personne->getPerNum(),PDO::PARAM_STR);
+		$req ->bindValue(':nom',$Personne->getPerNom(),PDO::PARAM_STR);
+		$req ->bindValue(':prenom',$Personne->getPerPrenom(),PDO::PARAM_STR);
+		$req ->bindValue(':tel',$Personne->getPerTel(),PDO::PARAM_STR);
+		$req ->bindValue(':mail',$Personne->getPerMail(),PDO::PARAM_STR);
+		$req ->bindValue(':login',$Personne->getPerLogin(),PDO::PARAM_STR);
+		$req ->bindValue(':pwd',$Personne->getPerPwd(),PDO::PARAM_STR);
+		
+		$req->execute();
+	}
 	
 }
