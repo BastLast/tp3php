aa18ef70ffa90015fa2d87f0682d152c9da7dfce
Unknown <jarreau.maxime@gmail.com>
2018-11-07 17:02:49 +0100

    complétion de la classe Personne et création du formulaire ajouter personne

--- classes/Personne.class.php
+++ classes/Personne.class.php
@@ -1,4 +1,82 @@
 <?php
 class Personne{
+	private $per_num;
+	private $per_nom;
+	private $per_prenom;
+	private $per_tel;
+	private $per_mail;
+	private $per_login;
+	private $per_pwd;
 	
+	public function __construct($valeur = array()){
+		if (!empty($valeur)){
+			$this ->affecte($valeur);
+		}
+		
+	}
+	
+	public function affecte($donees){
+		foreach($donnees as $attribut =>$valeur){
+			
+			switch($attribut){
+				case 'per_num': $this->setPerNum($valeur);
+				break;
+				case 'per_nom': $this->setPerNom($valeur);
+				break;
+				case 'per_prenom': $this->setPerPrenom($valeur);
+				break;
+				case 'per_tel': $this->setPerTel($valeur);
+				break;
+				case 'per_mail': $this->setPerMail($valeur);
+				break;
+				case 'per_login': $this->setPerLogin($valeur);
+				break;
+				case 'per_pwd': $this ->setPerPwd($valeur);
+				break;
+			}
+		}
+	}
+	public function setPerNum($perNum){
+		$this->per_num=$perNum;
+	}
+	public function setPerNom($perNom){
+		$this->per_nom=$perNom;
+	}
+	public function setPerPrenom($perPrenom){
+		$this->per_prenom=$perPrenom;
+	}
+	public function setPerTel($perTel){
+		$this->per_tel=$perTel;
+	}
+	public function setPerMail($perMail){
+		$this->per_mail=$perMail;
+	}
+	public function setPerLogin($perLogin){
+		$this->per_Login=$perLogin;
+	}
+	public function setPerPwd($perPwd){
+		$this->per_Pwd=$perPwd;
+	}
+	
+	public function getPerNum(){
+		return $this->per_Num;
+	}
+	public function getPerNom(){
+		return $this->per_Nom;
+	}
+	public function getPerPrenom(){
+		return $this->per_prenom;
+	}
+	public function getPerTel(){
+		return $this->per_tel;
+	}
+	public function getPerMail(){
+		return $this->per_mail;
+	}
+	public function getPerLogin(){
+		return $this->per_Login;
+	}
+	public function getPerPdw(){
+		return $this->per_pwd;
+	}
 }
--- include/pages/ajouterPersonne.inc.php
+++ include/pages/ajouterPersonne.inc.php
@@ -1,3 +1,19 @@
-
-				<h1>Ajouter une personne</h1>
-	
+<h1>Ajouter une personne</h1>
+<?php
+$db=new Mypdo();
+$villeManager=new PersonneManager($db);
+if (empty($_POST)){ 
+?>
+	<form action="#" id="FormPersonne" metho="post">
+	Nom: <input type="text" name="nom" size="4">
+	Prenom: <input type="text" name="prenom" size="4"><br>
+	Telephone: <input type="text" name="tel" size="4">
+	Mail: <input type="text" name="mail" size="4"><br>
+	Login: <input type="text" name="login" size="4">
+	Mot de passe: <input type="text" name="pdp" size="4"><br>
+	Categorie: <input type="radio" name="etudiant" size="4"> Etudiant
+	<input type="radio" name="personnel" size="4"> Personnel
+	<input type="submit" value="Valider">
+	</form>
+<?php	
+}	?>
