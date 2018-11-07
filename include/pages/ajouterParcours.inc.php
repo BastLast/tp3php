4c8c986b05e184d5880e126786c3041ff5821124
BastLast <bastien.last87@gmail.com>
2018-11-07 11:56:42 +0100

    Fin du formulaire d'ajout de parcours

--- include/pages/ajouterParcours.inc.php
+++ include/pages/ajouterParcours.inc.php
@@ -1,32 +1,33 @@
 <h1>Ajouter un parcours</h1>
 <?php
+
+$db = new Mypdo();
+$villeManager = new VilleManager($db);
 if (empty($_POST)){
   ?>
   <form action="#" id="FormParcours" method="post">
     Ville 1 :
     <select name="ville1">
       <?php
-      while ($lign = Mysqli_fetch_array($resu)) {
-        echo '<option value="'.$lign['NO_COMMANDE'].'">'.$lign['NO_COMMANDE'].'</option>';
+      $listeVilles = $villeManager->getList();
+      foreach ($listeVilles as $ville) {
+        echo '<option value="'.$ville->getNomVille().'">'.$ville->getNomVille().'</option>';
       }
       ?>
-      <input type="submit" value="Valider" />
-    </form>
-    <?php
-  }else{
-    ?>
-    <?php
-    $ville = new Ville($_POST);
-    $db = new Mypdo();
-    $villeManager = new VilleManager($db);
+      </select>
+      Ville 2 :
+      <select name="ville2">
+        <?php
+        foreach ($listeVilles as $ville) {
+          echo '<option value="'.$ville->getNomVille().'">'.$ville->getNomVille().'</option>';
+        }
+        ?>
+        </select>
+        Nombre de kilomètre(s) :<input type="text" name="vil_nom" size="4">
+        <input type="submit" value="Valider" />
+      </form>
+      <?php
+    }else{
 
-    $villeManager -> addVille($ville);
-
+    }
     ?>
-    <img src="image\valid.png" alt="confirmation validee">
-    La ville "<b><?php
-    echo $_POST["vil_nom"];
-    ?></b>" a bien été ajoutée
-    <?php
-  }
-  ?>
