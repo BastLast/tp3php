600e56757b1a512aa4a243241a5900f2bc872f1c
Unknown <jarreau.maxime@gmail.com>
2018-11-08 11:54:45 +0100

    ajout de addEtudiant dans EtudiantManager

--- classes/EtudiantManager.class.php
+++ classes/EtudiantManager.class.php
@@ -1,4 +1,19 @@
 <?php
 class EtudiantManager{
+	public function __construct($db){
+		$this->db = $db;
+	}
 	
+	//fonction permettant d'jouter une Etudiant 
+	public function addEtudiant($Etudiant){
+		$req=$this ->db->prepare
+		('INSERT INTO etudiant (per_num,dep_num,div_num) 
+		VALUES (:per_num,:dep_num,:div_num)');
+		
+		$req ->bindValue(':num',$Etudiant->getPerNum(),PDO::PARAM_STR);
+		$req ->bindValue(':nom',$Etudiant->getDepNum(),PDO::PARAM_STR);
+		$req ->bindValue(':prenom',$Etudiant->getDivNum(),PDO::PARAM_STR);
+		
+		$req->execute();
+	}
 }
