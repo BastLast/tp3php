7a1d80e7332e3a98065b45ee504de81888b419ed
BastLast <bastien.last87@gmail.com>
2018-11-08 11:33:00 +0100

    Ajout de la classe salarie

--- classes/Salarie.class.php
+++ classes/Salarie.class.php
@@ -1,4 +1,48 @@
 <?php
 class Salarie{
-	
+	private $per_num;
+	private $sal_telprof;
+	private $fon_num;
+
+	public function __construct($valeur = array()){
+		if (!empty($valeur)){
+			$this ->affecte($valeur);
+		}
+
+	}
+
+	public function affecte($donees){
+		foreach($donnees as $attribut =>$valeur){
+
+			switch($attribut){
+				case 'per_num': $this->setPerNum($valeur);
+				break;
+				case 'sal_telprof': $this->setSalTelprof($valeur);
+				break;
+				case 'fon_num': $this->setFonNum($valeur);
+				break;
+			}
+		}
+	}
+	public function setPerNum($perNum){
+		$this->per_num=$perNum;
+	}
+	public function setSalTelprof($saltelprof){
+		$this->sal_telprof=$saltelprof;
+	}
+	public function setFonNum($fonnum){
+		$this->fon_num=$fonnum;
+	}
+
+
+	public function getPerNum(){
+		return $this->per_Num;
+	}
+	public function getDepNum(){
+		return $this->sal_telprof;
+	}
+	public function getfonnum(){
+		return $this->fon_num;
+	}
+
 }
