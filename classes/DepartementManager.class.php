9d7e277fe1c86d5760e47bcce9eefc7ff76b98c7
BastLast <bastien.last87@gmail.com>
2018-11-11 12:07:18 +0100

    Ajout des managers de departement et de division

--- classes/DepartementManager.class.php
+++ classes/DepartementManager.class.php
@@ -1,4 +1,22 @@
 <?php
 class DepartementManager{
-	
+	public function __construct($db){
+		$this->db = $db;
+	}
+
+	//fonction permetant de lister tous les departements
+	public function getList(){
+
+		$listeDepartements = array();
+		$sql = 	'SELECT dep_num,dep_nom,vil_num FROM departement ORDER BY dep_nom';
+		$req = $this->db->query($sql);
+
+		while ($departement = $req->fetch(PDO::FETCH_OBJ)) {
+			$listeDepartements[]= new Departement($departement);
+		}
+
+		return $listeDepartements;
+		$req -> closeCursor();
+	}
+
 }
--- classes/DivisionManager.class.php
+++ classes/DivisionManager.class.php
@@ -1,4 +1,22 @@
 <?php
 class DivisionManager{
-	
+	public function __construct($db){
+		$this->db = $db;
+	}
+
+	//fonction permetant de lister toutes les divisions
+	public function getList(){
+
+		$listeDivisions = array();
+		$sql = 	'SELECT div_num,div_nom FROM division ORDER BY div_nom';
+		$req = $this->db->query($sql);
+
+		while ($division = $req->fetch(PDO::FETCH_OBJ)) {
+			$listeDivisions[]= new Division($division);
+		}
+
+		return $listeDivisions;
+		$req -> closeCursor();
+	}
+
 }
