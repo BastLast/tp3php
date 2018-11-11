7f967fc525fdb50fdd6b244463a287a00d20fea2
BastLast <bastien.last87@gmail.com>
2018-11-11 12:01:34 +0100

    Ajout des classes département et division

--- classes/Departement.class.php
+++ classes/Departement.class.php
@@ -1,4 +1,48 @@
 <?php
-class Departement{
-	
+class Etudiant{
+	private $dep_num;
+	private $dep_nom;
+	private $vil_num;
+
+	public function __construct($valeur = array()){
+		if (!empty($valeur)){
+			$this ->affecte($valeur);
+		}
+
+	}
+
+	public function affecte($donnees){
+		foreach($donnees as $attribut =>$valeur){
+
+			switch($attribut){
+				case 'dep_num': $this->setDepNum($valeur);
+				break;
+				case 'dep_num': $this->setDepNom($valeur);
+				break;
+				case 'vil_num': $this->setVilNum($valeur);
+				break;
+			}
+		}
+	}
+
+	public function setDepNum($depNum){
+		$this->dep_num=$depNum;
+	}
+	public function setDepNom($depNum){
+		$this->dep_num=$depNom;
+	}
+	public function setDepNum($vilNum){
+		$this->dep_num=$depnum;
+	}
+
+	public function getDepNum(){
+		return $this->dep_num;
+	}
+	public function getDepNom(){
+		return $this->dep_Nom;
+	}
+	public function getvilNum(){
+		return $this->vil_num;
+	}
+
 }
--- classes/Division.class.php
+++ classes/Division.class.php
@@ -1,4 +1,40 @@
 <?php
 class Division{
-	
+	private $div_num;
+	private $div_nom;
+
+	public function __construct($valeur = array()){
+		if (!empty($valeur)){
+			$this ->affecte($valeur);
+		}
+
+	}
+
+	public function affecte($donnees){
+		foreach($donnees as $attribut =>$valeur){
+
+			switch($attribut){
+				case 'div_num': $this->setDivNum($valeur);
+				break;
+				case 'div_nom': $this->setDivNom($valeur);
+				break;
+			}
+		}
+	}
+	public function setDivNum($divNum){
+		$this->div_num=$divNum;
+	}
+	public function setDivNom($divNom){
+		$this->div_nom=$divNom;
+	}
+
+
+	public function getDivNum(){
+		return $this->div_num;
+	}
+
+		public function getDivNom(){
+			return $this->div_nom;
+		}
+
 }
