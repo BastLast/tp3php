699173a7c044750bbef3e062339f7535165f4657
BastLast <bastien.last87@gmail.com>
2018-11-11 10:57:28 +0100

    Ajout de la classe Fonction

--- classes/Fonction.class.php
+++ classes/Fonction.class.php
@@ -1,4 +1,40 @@
 <?php
 class Fonction{
-	
+	private $fon_num;
+	private $fon_libelle;
+
+	public function __construct($valeur = array()){
+		if (!empty($valeur)){
+			$this ->affecte($valeur);
+		}
+
+	}
+
+	public function affecte($donees){
+		foreach($donnees as $attribut =>$valeur){
+
+			switch($attribut){
+				case 'fon_num': $this->setFonNum($valeur);
+				break;
+				case 'fon_libelle': $this->setFonLib($valeur);
+				break;
+			}
+		}
+	}
+	public function setFonNum($fonNum){
+		$this->fon_num=$fonNum;
+	}
+	public function setFonLib($fonLib){
+		$this->fon_libelle=$fonnum;
+	}
+
+
+	public function getfonNum(){
+		return $this->fon_num;
+	}
+
+		public function getfonLib(){
+			return $this->fon_libelle;
+		}
+
 }
