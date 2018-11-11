16ab2f16d6683c0cae2d3ff6504a8f068aa453e4
BastLast <bastien.last87@gmail.com>
2018-11-11 09:38:06 +0100

    Ajout du manager d'étudiants et de salariés

--- classes/EtudiantManager.class.php
+++ classes/EtudiantManager.class.php
@@ -3,17 +3,17 @@ class EtudiantManager{
 	public function __construct($db){
 		$this->db = $db;
 	}
-	
-	//fonction permettant d'jouter une Etudiant 
+
+	//fonction permettant d'ajouter une Etudiant
 	public function addEtudiant($Etudiant){
 		$req=$this ->db->prepare
-		('INSERT INTO etudiant (per_num,dep_num,div_num) 
+		('INSERT INTO etudiant (per_num,dep_num,div_num)
 		VALUES (:per_num,:dep_num,:div_num)');
-		
+
 		$req ->bindValue(':per_num',$Etudiant->getPerNum(),PDO::PARAM_STR);
 		$req ->bindValue(':dep_num',$Etudiant->getDepNum(),PDO::PARAM_STR);
 		$req ->bindValue(':div_num',$Etudiant->getDivNum(),PDO::PARAM_STR);
-		
+
 		$req->execute();
 	}
 }
--- classes/SalarieManager.class.php
+++ classes/SalarieManager.class.php
@@ -1,4 +1,20 @@
 <?php
 class SalarieManager{
-	
+	public function __construct($db){
+		$this->db = $db;
+	}
+
+	//fonction permettant d'jouter une ville
+	public function addSalarie($Salarie){
+		$req=$this ->db->prepare
+		('INSERT INTO salarie (per_num,sal_telprof,fon_num)
+		VALUES (:num,:saltelprof,:fonnum)');
+
+		$req ->bindValue(':num',$Personne->getSalNum(),PDO::PARAM_STR);
+		$req ->bindValue(':saltelprof',$Personne->getSalTelProf(),PDO::PARAM_STR);
+		$req ->bindValue(':fonnum',$Personne->getSalFonNum(),PDO::PARAM_STR);
+
+		$req->execute();
+	}
+
 }
