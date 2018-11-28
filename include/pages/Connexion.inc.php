48dc5e92a939cc70f98c58f3c08fd11989bae51a
BastLast <bastien.last87@gmail.com>
2018-11-28 08:53:03 +0100

    Ajout Deconnexion et redirection connexion

--- include/pages/Connexion.inc.php
+++ include/pages/Connexion.inc.php
@@ -1,33 +1,40 @@
-<h1>Pour vous connecter</h1>
+
 <?php
 $db=new Mypdo();
 $personneManager= new PersonneManager($db);
 if (empty($_POST)){
-  ?>
-  <form action="#" id="FormLogin" method="post">
-    Nom d'utilisateur:<input type="text" name="login" size="4">
-    Mot de passe:<input type="password" name="password" size="4">
+  if(isset($_SESSION['co'])){
+    ?>
+    Vous êtes déjà connecté, vous allez être redirigé dans 2 secondes !
+    <meta http-equiv="refresh" content="2; URL=index.php?page=0">
+    <?php
+  }else {
+    ?>
+    <h1>Pour vous connecter</h1>
+    <form action="#" id="FormLogin" method="post">
+      Nom d'utilisateur:<input type="text" name="login" size="4">
+      Mot de passe:<input type="password" name="password" size="4">
+
+      <?php
+      $nb2 = rand(1 , 9);
+      $nb1 = rand(1 , 9);
+      $_SESSION['verifCaptcha'] = $nb1 + $nb2;
+      ?>
+      <img src=<?php echo "image/nb/$nb1.jpg" ?> alt="captcha1">
+      +
+      <img src=<?php echo "image/nb/$nb2.jpg" ?> alt="captcha2">
+      =
+      <?php
+      ?>
+      <input type="text" name="captcha" size="4">
+      <input type="submit" value="Valider" />
+    </form>
+
 
     <?php
-    $nb2 = rand(1 , 9);
-    $nb1 = rand(1 , 9);
-    $_SESSION['verifCaptcha'] = $nb1 + $nb2;
-    ?>
-    <img src=<?php echo "image/nb/$nb1.jpg" ?> alt="captcha1">
-    +
-    <img src=<?php echo "image/nb/$nb2.jpg" ?> alt="captcha2">
-    =
-    <?php
-    ?>
-    <input type="text" name="captcha" size="4">
-    <input type="submit" value="Valider" />
-  </form>
-
-
-  <?php
+  }
 }else{
-  ?>
-  <?php
+
   if ($_POST['captcha']!=$_SESSION['verifCaptcha']) { // le captcha est invalide
     ?>
     <img src="image/erreur.png" alt="connexion echouee">
@@ -41,6 +48,11 @@ if (empty($_POST)){
     if($personne -> checkPassword($_POST['password'])){
       // le mot de passe est OK
       $_SESSION['co'] = 1;
+      ?>
+      <h1>Connexion Réussie</h1>
+      Vous allez etre redirigé dans 2 secondes !
+      <meta http-equiv="refresh" content="2; URL=index.php?page=0">
+      <?php
     }else{
       // le mot de passe n'est pas OK
       ?>
--- include/pages/Deconnexion.inc.php
+++ include/pages/Deconnexion.inc.php
@@ -1,2 +1,6 @@
-<?php 
-	
+<?php
+	unset($_SESSION['co']);
+	?>
+	<h1>Déonnexion Réussie</h1>
+	Vous allez etre redirigé dans 2 secondes !
+	<meta http-equiv="refresh" content="2; URL=index.php?page=0">
