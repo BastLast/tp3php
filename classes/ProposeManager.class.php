81618e1e3479865fa6564e32101d0445e45e5209
Unknown <jarreau.maxime@gmail.com>
2018-11-29 22:50:56 +0100

    ajout de supPropose

--- classes/ProposeManager.class.php
+++ classes/ProposeManager.class.php
@@ -1,5 +1,20 @@
 <?php
 class ProposeManager{
 	
+	
+	
+	public function supPropose($id){
+		
+		if(!is_null($id)){
+		
+		$req=$this->db->prepare(
+			'DELETE FROM propose WHERE per_num = :id || per_per_num= :id'	
+		);
+		
+		$req->bindValue(':id',$id,PDO::PARAM_STR);
+		return $req->execute();
+		$req->closeCursor;
+		}
+	}
 
 }
