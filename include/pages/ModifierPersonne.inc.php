b1ed2354514969535041eff85c7b7c7285cdcb15
BastLast <bastien.last87@gmail.com>
2018-12-01 22:47:13 +0100

    Ajout préremplissage modifier personne

--- include/pages/ModifierPersonne.inc.php
+++ include/pages/ModifierPersonne.inc.php
@@ -6,7 +6,7 @@ if(empty($_POST)){ //premier passage sur la page
 	<form method="post" action="#" id="FormModifierPersonne" >
 		Personne a modifier :
 		<select name="personneModifiee" onChange='javascript:document.getElementById("FormModifierPersonne").submit()'>
-			 <option value="Defaut">Choisissez une personne</option>
+			<option value="Defaut">Choisissez une personne</option>
 			<?php
 			foreach($listePersonne as $personne){
 				echo '<option value="'.$personne->getPerNum().'">'.$personne->getPerNom()." ".$personne->getPerPrenom().'</option>';
@@ -17,84 +17,104 @@ if(empty($_POST)){ //premier passage sur la page
 	</form>
 
 	<?php
+}else{ //ce n'est pas le premier passage
 
-
-}else{ //deuxieme passage sur la page
-
-	$_SESSION['idPersonneAModifier'] =$_POST['personneModifiee'];
-	$personne = new Personne($personneManager->getPersonneById( $_SESSION['idPersonneAModifier'] ));
-	
-	if(empty($_POST['nom'])){
-
-
-	?>
+	if(empty($_POST['nom'])){ //deuxieme passage sur la page
+		$_SESSION['personne'] = $personneManager->getPersonneById( $_POST['personneModifiee'] );
+		?>
 		<form action="##" id="FormModifierPersonne" method="post">
-			<?php var_dump($personne);
 
-			?>
 			Nom:
-			<?php echo '<input type="text" name="nom" size="4" value= "'.$personne->getPerNom().'" required>'			?>
-			Prenom: <input type="text" name="prenom" size="4" value= "<?php echo $personne->getPerPrenom(); ?>" required><br>
-			Telephone: <input type="text" name="tel" size="4" value= "<?php echo $personne->getPerTel(); ?>" required >
-			Mail: <input type="email" name="mail" size="4" value= "<?php echo $personne->getPerMail(); ?>" required> <br>
-			Login: <input type="text" name="login" size="4" value= "<?php echo $personne->getPerLogin(); ?>" required>
-			Mot de passe: <input type="password" name="pdp" size="4"><br>
+			<?php echo '<input type="text" name="nom" size="4" value= "'.$_SESSION['personne']->getPerNom().'" required>'			?>
+			Prenom: <input type="text" name="prenom" size="4" value= "<?php echo $_SESSION['personne']->getPerPrenom(); ?>" required><br>
+			Telephone: <input type="text" name="tel" size="4" value= "<?php echo $_SESSION['personne']->getPerTel(); ?>" required >
+			Mail: <input type="email" name="mail" size="4" value= "<?php echo $_SESSION['personne']->getPerMail(); ?>" required> <br>
+			Login: <input type="text" name="login" size="4" value= "<?php echo $_SESSION['personne']->getPerLogin(); ?>" required>
+			Mot de passe: <input type="password" name="pdp" size="4" required><br>
 			Categorie:
 
-			<?php if($personneManager->estEtudiant($_SESSION['idPersonneAModifier'] )) { ?>
-				 <input type="radio" name="type" value="etudiant" size="4" checked="checked"> Etudiant
-				 <input type="radio" name="type" value="personnel" size="4" > Personnel
-			<?php } else{ ?>
+			<?php if($personneManager->estEtudiantByid($_SESSION['personne']->getPerNum())) {
+				//test si la personne est un étudiant ou un salarié ?>
+				<input type="radio" name="type" value="etudiant" size="4" checked="checked"> Etudiant
+				<input type="radio" name="type" value="personnel" size="4" > Personnel
+			<?php } else{
+				//la personne est un personnel ?>
 				<input type="radio" name="type" value="etudiant" size="4" > Etudiant
 				<input type="radio" name="type" value="personnel" size="4" checked="checked"> Personnel
 			<?php } ?>
 			<input type="submit" value="Valider">
 		</form>
-	<?php
-	}else{
+		<?php
 
-			if(!$personneManager->estEtudiant($_SESSION['idPersonneAModifier'] )){
+	}else{ // ce n'est pas le premier ou le deuxieme passage
+		if(isset($_POST['type'])){
+			//c'est le troisieme passage
+			$_SESSION['newType'] = $_POST['type']; //sauvegarde du choix
 
-					?>
-					<form id="FormPersonnel" method="post">
-						Telephone professionnel: <input type="text" name="telpro">
-						Fonction: <select name="fonction">
-							<?php
-							$listeFonctions = $fonctionManager->getList();
-							foreach ($listeFonctions as $fonction) {
-								echo '<option value="'.$fonction->getFonNum().'">'.$fonction->getFonLib().'</option>';
+			if($_POST['type'] == "personnel"){
+				$_SESSION['salarie'] = $salarieManager->getSalarieById($_SESSION['personne']->getPerNum());
+				//la personne à modifier est ou doit devenir un membre du personnel
+				?>
+				<form id="FormPersonnel" method="post">
+					Telephone professionnel: <input type="text" name="telpro" value="<?php echo $_SESSION['salarie']->getTelProf()  ?>" >
+					Fonction: <select name="fonction">
+						<?php
+						$listeFonctions = $fonctionManager->getList();
+						foreach ($listeFonctions as $fonction) {
+							if ($_SESSION['salarie']->getFonNum() == $fonction->getFonNum()) {
+								echo '<option selected';
+							}else{
+								echo '<option';
 							}
-							?>
-						</select>
-						<input type="submit" value="Valider">
-					</form>
-					<?php
+							echo '  value="'.$fonction->getFonNum().'">'.$fonction->getFonLib().'</option>';
+
+						}
+						?>
+					</select>
+					<input type="submit" value="Valider">
+				</form>
+				<?php
 			}
-			if($personneManager->estEtudiant($_SESSION['idPersonneAModifier'] )){
-
-					?>
-					<form id="FormEtudiant" method="post">
-						Annee: <select name="annee">
-							<?php
-							$listeDivisions = $divisionManager->getList();
-							foreach ($listeDivisions as $division) {
-								echo '<option value="'.$division->getDivNum().'">'.$division->getDivNom().'</option>';
+			else{
+				$_SESSION['etudiant'] = $etudiantManager->getEtudiantById($_SESSION['personne']->getPerNum());
+				//la personne à modifier est ou doit devenir un étudiant
+				?>
+				<form id="FormEtudiant" method="post">
+					Annee: <select name="annee">
+						<?php
+						$listeDivisions = $divisionManager->getList();
+						foreach ($listeDivisions as $division) {
+							if ($_SESSION['etudiant']->getDivNum() == $division->getDivNum()) {
+								echo '<option selected';
+							}else{
+								echo '<option';
 							}
-							?>
-						</select>
-						Departement: <select name="dep">
-							<?php
-							$listeDepartements = $departementManager->getList();
-							foreach ($listeDepartements as $departement) {
-								echo '<option value="'.$departement->getDepNum().'">'.$departement->getDepNom().'</option>';
+							echo ' value="'.$division->getDivNum().'">'.$division->getDivNom().'</option>';
+						}
+						?>
+					</select>
+					Departement: <select name="dep">
+						<?php
+						$listeDepartements = $departementManager->getList();
+						foreach ($listeDepartements as $departement) {
+							if ($_SESSION['etudiant']->getDepNum() == $departement->getDepNum()) {
+								echo '<option selected';
+							}else{
+								echo '<option';
 							}
-							?>
-						</select>
-						<input type="submit" value="Valider">
-					</form>
-					<?php
+							echo ' value="'.$departement->getDepNum().'">'.$departement->getDepNom().'</option>';
+						}
+						?>
+					</select>
+					<input type="submit" value="Valider">
+				</form>
+				<?php
 			}
 		}
+		else{
+			//c'est le 4 eme passage
+		}
+	}
 	unset($_SESSION['idPersonneAModifier']);
 }
 ?>
