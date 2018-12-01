91d38978fc25f7d5aa11e8bfb55957300d5937d2
BastLast <bastien.last87@gmail.com>
2018-12-01 17:03:56 +0100

    Ajout de la procedure d'ajout de trajet

--- classes/ProposeManager.class.php
+++ classes/ProposeManager.class.php
@@ -1,21 +1,36 @@
 <?php
 class ProposeManager{
-	
+
 	public function __construct($db){
 		$this->db = $db;
 	}
-	
-	public function supPropose($id){
-		
+
+	//fonction permettant d'jouter une proposition de trajet
+	public function addPropose($Propose){
+		$req=$this ->db->prepare
+		('INSERT INTO propose (per_num,pro_date,pro_time,pro_place,pro_sens)
+		VALUES (:num,:prodate,:protime,:places,:login)');
+
+		$req ->bindValue(':num',$Propose->getPerNum(),PDO::PARAM_STR);
+		$req ->bindValue(':prodate',$Propose->getPerPrenom(),PDO::PARAM_STR);
+		$req ->bindValue(':protime',$Propose->getPerTel(),PDO::PARAM_STR);
+		$req ->bindValue(':places',$Propose->getPerMail(),PDO::PARAM_STR);
+		$req ->bindValue(':sens',$Propose->getPerLogin(),PDO::PARAM_STR);
+
+
+		$req->execute();
+	}
+	public function supProposeOfPersonByid($id){
+
 		if(isset($id)){
-		
-		$req=$this->db->prepare(
-			'DELETE FROM propose WHERE per_num = :id'	
-		);
-		
-		$req->bindValue(':id',$id,PDO::PARAM_STR);
-		return $req->execute();
-		$req->closeCursor;
+			
+			$req=$this->db->prepare(
+				'DELETE FROM propose WHERE per_num = :id'
+			);
+
+			$req->bindValue(':id',$id,PDO::PARAM_STR);
+			$req->execute();
+			$req->closeCursor;
 		}
 	}
 
